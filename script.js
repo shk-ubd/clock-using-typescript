1d36446d2715ecad744aa0ff645a0078af02a0cc
Ubaid <ubaidullah.gr8@gmail,com>
2023-06-28 19:59:45 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var D;
+var day, date;
+var mm, hh, ss;
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var display = function () {
+    D = new Date;
+    date = "".concat(D.getDate(), " ").concat(months[D.getMonth()], ", ").concat(D.getFullYear());
+    day = "".concat(days[D.getDay()]);
+    hh = D.getHours();
+    mm = D.getMinutes();
+    ss = D.getSeconds();
+    document.getElementById("date").innerHTML = date;
+    document.getElementById("day").innerHTML = day;
+    document.getElementById("hours").innerHTML = (hh > 12 ? hh - 12 : hh).toString().padStart(2, "0");
+    document.getElementById("minutes").innerHTML = mm.toString().padStart(2, "0");
+    document.getElementById("seconds").innerHTML = ss.toString().padStart(2, "0");
+};
+setInterval(display, 1000);
---
+++ script.ts
@@ -0,0 +1,24 @@
+let D: Date;
+let day: string, date: string;
+let mm: number, hh: number, ss: number;
+
+let months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let days: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+let display = () => {
+    D = new Date;
+    date = `${D.getDate()} ${months[D.getMonth()]}, ${D.getFullYear()}`
+    day = `${days[D.getDay()]}`
+    hh = D.getHours();
+    mm = D.getMinutes();
+    ss = D.getSeconds();
+
+    (document.getElementById("date") as HTMLElement).innerHTML = date;
+    (document.getElementById("day") as HTMLElement).innerHTML = day;
+    (document.getElementById("hours") as HTMLElement).innerHTML = (hh > 12 ? hh - 12 : hh).toString().padStart(2, "0");
+    (document.getElementById("minutes") as HTMLElement).innerHTML = mm.toString().padStart(2, "0");
+    (document.getElementById("seconds") as HTMLElement).innerHTML = ss.toString().padStart(2, "0");
+
+
+}
+setInterval(display, 1000)
